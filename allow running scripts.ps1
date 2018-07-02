9b55c3fa91d653636a39923653837ee559deeeed
RJK-Engineering <rjklinkhamer@gmail.com>
2018-07-02 09:45:57 +0200

    Added -Force

--- allow running scripts.ps1
+++ allow running scripts.ps1
@@ -1,2 +1,2 @@
 # http://go.microsoft.com/fwlink/?LinkID=135170
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
